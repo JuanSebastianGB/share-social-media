1dc226884778aa369fb3ba4bef0abcfcc1f89036
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2022-12-25 14:30:45 -0500

    Fix: Fixing default post model properties

--- server/models/post.js
+++ server/models/post.js
@@ -6,8 +6,8 @@ const PostSchema = new mongoose.Schema(
     body: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
     fileId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    likes: { type: Map, of: Boolean },
-    comments: { type: Array, default: [] },
+    likes: { type: Map, of: Boolean, default: {} },
+    comments: { type: Array, of: String, default: [] },
   },
   { timestamps: true, versionKey: false }
 );
