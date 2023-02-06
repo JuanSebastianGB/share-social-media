14662b5eb81faf23b01eaf3ddaebb91926199f68
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-02-05 22:02:47 -0500

    Fix: Adding the cloudinary logic to register route too.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import { login, register } from '../controllers/auth.js';
+import cloudUpload from '../utilities/cloudinary.js';
 import uploadMiddleware from '../utilities/handleUploadFile.js';
 import { validatorLogin, validatorRegister } from '../validators/auth.js';
+
 const router = express.Router();
 
 router.post(
   '/register',
   uploadMiddleware.single('myFile'),
+  cloudUpload.uploadToCloud,
   validatorRegister,
   register
 );
